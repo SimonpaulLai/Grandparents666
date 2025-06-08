b691ac64ecf8a3a56bcdca232874a4696d43fa72
SimonpaulLai <133687464+SimonpaulLai@users.noreply.github.com>
2025-06-08 17:36:05 +0800

    Update Gen_Graph.py

--- Gen_Graph.py
+++ Gen_Graph.py
@@ -1,15 +1,13 @@
 import os
-from dotenv import load_dotenv
 import replicate
 from GPT_api_client import call_chat_api
-import streamlit as st
 
-api_key = st.secrets["REPLICATE_API_KEY"]
+api_key = os.getenv("REPLICATE_API_KEY")
 if api_key is None:
-    raise ValueError("請先設定環境變數 REPLICATE_API_KEY")
+    raise ValueError("Render環境請設定 REPLICATE_API_KEY")
 os.environ["REPLICATE_API_TOKEN"] = api_key
 
-def generate_image_from_diary(diary_text: str, output_path: str = "output.png") -> str:
+def generate_image_from_diary(diary_text: str, output_path: str = "/tmp/output.png") -> str:
     prompt = (
         f"An elderly man named 阿水伯 in a nursing home, "
         f"{diary_text}, anime style, soft lighting, Studio Ghibli vibe, "
